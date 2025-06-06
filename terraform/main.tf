7acf9fe35c0d9019631fa4d7530b02149f738c38
antoinedfr <antoine.difiore@gmail.com>
2025-06-06 10:50:57 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -275,21 +275,3 @@ resource "aws_instance" "db_instance" {
   }
 }
 
-########################
-# Outputs
-########################
-
-output "web_public_ip" {
-  description = "Public IP of the Web (frontend) instance"
-  value       = aws_instance.web_instance.public_ip
-}
-
-output "app_private_ip" {
-  description = "Private IP of the App (backend) instance"
-  value       = aws_instance.app_instance.private_ip
-}
-
-output "db_private_ip" {
-  description = "Private IP of the DB instance"
-  value       = aws_instance.db_instance.private_ip
-}
