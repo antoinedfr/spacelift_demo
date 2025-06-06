a9e0d727577efdadfb77ad41586ba87f4e03c646
antoinedfr <antoine.difiore@gmail.com>
2025-06-06 10:29:28 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,31 @@
-output "key_ssm_param" {
-  value = aws_ssm_parameter.private_key.name
+# SSM Parameter names for each private key
+output "web_private_key_ssm_name" {
+  description = "SSM parameter name for Web tier private key"
+  value       = aws_ssm_parameter.web_private_key.name
 }
 
-output "public_ip" {
-  value = aws_instance.demo_instance.public_ip
+output "app_private_key_ssm_name" {
+  description = "SSM parameter name for App tier private key"
+  value       = aws_ssm_parameter.app_private_key.name
+}
+
+output "db_private_key_ssm_name" {
+  description = "SSM parameter name for DB tier private key"
+  value       = aws_ssm_parameter.db_private_key.name
+}
+
+# Public and Private IPs of EC2 Instances
+output "web_public_ip" {
+  description = "Public IP of the Web (frontend) instance"
+  value       = aws_instance.web_instance.public_ip
+}
+
+output "app_private_ip" {
+  description = "Private IP of the App (backend) instance"
+  value       = aws_instance.app_instance.private_ip
+}
+
+output "db_private_ip" {
+  description = "Private IP of the DB instance"
+  value       = aws_instance.db_instance.private_ip
 }
